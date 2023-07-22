d830954e2eb50003c010da80749ecf0948ea9ab5
mike <mourav2222@gmail.com>
2023-07-23 00:04:48 +0200

    Initial commit

---
+++ src/test/java/qa/guru/allure/GitHubTest.java
@@ -0,0 +1,31 @@
+package qa.guru.allure;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.byLinkText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GitHubTest {
+
+    @Test
+    void searchForIssue() {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+        $(".header-search-button ").click();
+//        $(byId("query-builder-test")).sendKeys("allure-example");
+
+        $("#query-builder-test").sendKeys("allure-example");
+        $("#query-builder-test").submit();
+
+        $(byLinkText("eroshenkoam/allure-example")).click();
+        $("#issues-tab").click();
+        $(withText("#81")).should(exist);
+    }
+}
