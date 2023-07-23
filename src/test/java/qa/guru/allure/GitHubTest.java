b9ceb203a56a8ce0423b6c45983044633e628b91
mike <mourav2222@gmail.com>
2023-07-23 15:50:04 +0200

    add steps in more tests

--- src/test/java/qa/guru/allure/GitHubTest.java
+++ src/test/java/qa/guru/allure/GitHubTest.java
@@ -3,6 +3,8 @@ package qa.guru.allure;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exist;
@@ -10,17 +12,26 @@ import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class GitHubTest {
 
-    @Test
-    void searchForIssue() {
+    @BeforeAll
+    static void setup() {
 
-        String idea = System.getenv("idea");
+        String idea = System.getProperty("idea");
         System.out.println("idea: " + idea);
 
-        Configuration.remote = "http://localhost:4444/wd/hub";
+        if(!Boolean.valueOf(idea)) {
+            Configuration.remote = "http:/localhost:4444/wd/hub";
+        }
         SelenideLogger.addListener("allure", new AllureSelenide());
+
+    }
+
+    @Test
+    @DisplayName("Search for issue with number 74")
+    void searchForIssue() {
 
         open("https://github.com");
         $(".header-search-button ").click();
@@ -31,6 +42,39 @@ public class GitHubTest {
 
         $(byLinkText("eroshenkoam/allure-example")).click();
         $("#issues-tab").click();
-        $(withText("#81")).should(exist);
+        $(withText("#74")).should(exist);
+    }
+
+    @Test
+    @DisplayName("Search for issue with number 80")
+    void searchForIssue1() {
+
+
+        step("Open main page", () -> open("https://github.com"));
+
+        step("Search for repository", () -> {
+            $(".header-search-button ").click();
+            $("#query-builder-test").sendKeys("allure-example");
+            $("#query-builder-test").submit();
+        });
+        step("Open repository by link", () -> $(byLinkText("eroshenkoam/allure-example")).click());
+
+        step("Open issue tab", ()-> $("#issues-tab").click());
+
+        step("Should see issue with number 80", () -> $(withText("#80")).should(exist));
+    }
+
+    @Test
+    @DisplayName("Search for issue with number 81")
+    void searchForIssue2() {
+
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForReository("allure-example");
+        steps.openRepositoryByLink("eroshenkoam/allure-example");
+        steps.openIssueTab();
+        steps.shouldSeeIssueWithNumber(80);
+
     }
 }
---
+++ src/test/java/qa/guru/allure/WebSteps.java
@@ -0,0 +1,42 @@
+package qa.guru.allure;
+
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.byLinkText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class WebSteps {
+
+    @Step("Open main page")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Search for repository {repo}")
+    public void searchForReository(String repo) {
+        $(".header-search-button ").click();
+        $("#query-builder-test").sendKeys(repo);
+        $("#query-builder-test").submit();
+    }
+
+    @Step("Open repository by link {link}")
+    public void openRepositoryByLink(String link) {
+        $(byLinkText(link)).click();
+    }
+
+    @Step("Open issue tab")
+    public void openIssueTab() {
+
+        $("#issues-tab").click();
+    }
+
+    @Step("Should see issue with number {num}")
+    public void shouldSeeIssueWithNumber(int num) {
+        $(withText("#" + num)).should(exist);
+
+    }
+}
+
